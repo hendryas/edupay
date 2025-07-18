04de5831726aa86f122bde356ec9d55644b47334
Hendry Agus Setiawan <hendryas321@gmail.com>
2025-07-18 23:28:30 +0700

    Edupay V1.1

--- resources/views/admin/tagihan/pendaftaran.blade.php
+++ resources/views/admin/tagihan/pendaftaran.blade.php
@@ -168,11 +168,9 @@
     <!-- DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-
     <script>
         $(document).ready(function() {
             $('#pendaftaranTable').DataTable();
         });
     </script>
-
 @endsection
